36a7a124a6c94d18238a4360ac8d8cbbcc32b79f
Petr Kvasnica <petrkvasnica@gmail.com>
2025-07-27 21:50:05 +0200

    new start

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-const SITE = require('./src/config.js').SITE;
-
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: `${SITE.origin}${SITE.basePathname}`,
-  generateRobotsTxt: true,
-};
