7aeb140f65f0b55d5c0236a6e866aa6a3e87e891
Petr Kvasnica <petrkvasnica@gmail.com>
2025-12-19 07:47:04 +0100

    fix: make next16 build pass (turbopack config + remove eslint key)

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 module.exports = withBundleAnalyzer({
-  eslint: {
-    dirs: ['.'],
-  },
+  turbopack: {},
   poweredByHeader: false,
   trailingSlash: true,
   basePath: '',
